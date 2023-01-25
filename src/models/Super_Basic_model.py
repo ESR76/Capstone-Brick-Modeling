3c51d23ec5f0e6205c85d624e3588d3908eba6f5
Jbomwell <88361935+Jbomwell@users.noreply.github.com>
2023-01-24 17:55:57 -0800

    new linear regression model
    now has day, month, etc columns. didn't seem to change the results by much. Linear regression is prob not the move.

--- src/models/Super_Basic_model.py
+++ src/models/Super_Basic_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# In[106]:
+# In[50]:
 
 
 import brickschema
@@ -11,107 +11,122 @@ from sklearn.linear_model import LinearRegression
 from sklearn.metrics import mean_squared_error
 
 
-# In[107]:
+# In[51]:
 
 
 #testing stuff
 
 
-# In[108]:
+# In[52]:
 
 
 
-df = pandas.read_csv('merged_all_2F.csv')
+df = pd.read_csv('merged_all_2F.csv')
 
 
-# In[109]:
+# In[53]:
 
 
 print(df['time'])
 
 
-# In[114]:
+# In[110]:
 
 
 def temp_conversion(val):
     return (val - 32) * 5/9 + 273.15
 
-df['time'] = df['time'].transform(pd.Timestamp)
+df['time'] = pd.to_datetime(df['time'], format='%Y-%m-%d %H:%M:%S')
 df['Outside Air Temp'] = temp_conversion(df['Outside Air Temp'])
-saf = df['Actual Supply Flow']
 df['Zone Temperature'] = temp_conversion(df['Zone Temperature'])
 
+#datestuff
+df['year'] = df['time'].dt.year
+df['hour'] = df['time'].dt.hour
+df['month'] = df['time'].dt.month
+df['day'] = df['time'].dt.day
+df['minute'] = df['time'].dt.minute
 
-# In[115]:
+
+# In[111]:
 
 
 trainstart = df[:len(df)//2]
 teststart = df[len(df)//2:]
 
 
-# In[116]:
+# In[112]:
 
 
 #cleaned data
-train = trainstart[['time','Common Setpoint','Actual Sup Flow SP','Zone Temperature','Actual Supply Flow','energy','Humidity', 'Outside Air Temp']]
-test = teststart[['time','Common Setpoint','Actual Sup Flow SP','Zone Temperature','Actual Supply Flow','energy','Humidity', 'Outside Air Temp']]
+train = trainstart[['Common Setpoint','Actual Sup Flow SP','Zone Temperature',                    'Actual Supply Flow','energy','Humidity', 'Outside Air Temp',                    'year','day','hour','month','minute']]
+test = teststart[['Common Setpoint','Actual Sup Flow SP','Zone Temperature',                  'Actual Supply Flow','energy','Humidity', 'Outside Air Temp',                  'year','day','hour','month','minute']]
 
 
-# In[117]:
+# In[113]:
 
 
 train
 
 
-# In[ ]:
-
-
-
-
-
-# In[120]:
+# In[114]:
 
 
 regr = LinearRegression()
 #Xtrain = train.drop(columns=['energy'])
-Xtrain = train.drop(columns=['energy','time'])
+Xtrain = train.drop(columns=['energy'])
 Ytrain = train[['energy']]
-Xtest = test.drop(columns=['energy','time'])
+Xtest = test.drop(columns=['energy'])
 Ytest = test[['energy']]
 
 
-# In[121]:
+# In[115]:
 
 
 regr.fit(Xtrain, Ytrain)
 
 
-# In[122]:
+# In[116]:
 
 
 y_pred = regr.predict(Xtest)
 
 
-# In[123]:
+# In[117]:
 
 
 y_pred
 
 
-# In[124]:
+# In[118]:
 
 
 Ytest
 
 
-# In[125]:
+# In[119]:
 
 
 
 mean_squared_error(np.array(Ytest), y_pred)
 
 
-# In[ ]:
+# In[132]:
+
+
+regr = LinearRegression()
+#Xtrain = train.drop(columns=['energy'])
+Xtrain = train.drop(columns=['energy','month'])
+Ytrain = train[['energy']]
+Xtest = test.drop(columns=['energy','month'])
+Ytest = test[['energy']]
+regr.fit(Xtrain, Ytrain)
+y_pred = regr.predict(Xtest)
+mean_squared_error(np.array(Ytest), y_pred)
+#month seems to worsen the result
+
+
+# In[125]:
 
 
 
