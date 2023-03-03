316d7ecb1581129c7c70116626cc5fa6493655f8
ESR76 <esperanzasofia@gmail.com>
2023-03-03 14:58:03 -0800

    Working on more visualization code

--- src/visualization/visualize.py
+++ src/visualization/visualize.py
@@ -16,6 +16,41 @@ def visualize_results(cwd, opt_results, is_train, **params):
 		print("\nin run -> visualize for test data")
 		direc = params['test_directory']
 
+
+	### VISUALIZATION OF TRENDS BY UNIT OF TIME
+	plot_df = pd.read_csv(cwd + direc + params['orig_name'])
+
+	plot_df.loc[:, params['time_col']] = plot_df.loc[:, params['time_col']].transform(pd.Timestamp)
+	plot_df.loc[:, 'hour'] = plot_df.loc[:, params['time_col']].transform(lambda x: x.hour)
+	plot_df.loc[:, 'weekday'] = plot_df.loc[:, params['time_col']].transform(lambda x: x.weekday)
+	#plot_df.loc[:, 'weekday'] = plot_df.loc[:, 'weekday'].replace({0: 'Mon', 1: 'Tues', 2: 'Wed', 3: 'Thurs', 4: 'Fri', 5: 'Sat', 6: 'Sun'})
+	plot_df.loc[:, 'month'] = plot_df.loc[:, params['time_col']].transform(lambda x: x.month)
+	#plot_df.loc[:, 'month'] = plot_df.loc[:, 'month'].replace({1: 'Jan', 2: 'Feb', 3: 'Mar', 4: 'Apr', 5: 'May', 6: 'Jun', 7: 'Jul', 8: 'Aug', 9: 'Sept', 10: 'Oct', 11: 'Nov', 12: 'Dec'})
+	#plot_df.loc[:, 'year'] = plot_df.loc[:, params['time_col']].transform(lambda x: x.year)
+	#plot_df.loc[:, 'month/year'] = plot_df.apply(lambda x: str(x['month']) + '/' + str(x['year']), axis = 1)
+
+	# LOOK INTO THIS FOR LABELS: https://stackoverflow.com/questions/62630875/how-to-change-the-plot-order-of-the-categorical-x-axis
+
+	#mo_year_groups = plot_df.groupby('month/year')['energy'].mean()
+	month_groups = plot_df.groupby('month')['energy'].mean()
+	weekday_groups = plot_df.groupby('weekday')['energy'].mean()#.replace({0: 'Mon', 1: 'Tues', 2: 'Wed', 3: 'Thurs', 4: 'Fri', 5: 'Sat', 6: 'Sun'})
+	hour_groups = plot_df.groupby('hour')['energy'].mean()
+
+	fig, axes = plt.subplots(nrows=3, ncols=1, dpi=120, figsize=(18,6))
+
+	sns.lineplot(data = hour_groups.reset_index(), x = 'hour', y = 'energy', ax = axes[0])
+	axes[0].set_title('Hour Means for Energy')
+	sns.lineplot(data = weekday_groups.reset_index(), x = 'weekday', y = 'energy', ax = axes[1], sort = False)
+	axes[1].set_title('Weekday Means for Energy')
+	sns.lineplot(data = month_groups.reset_index(), x = 'month', y = 'energy', ax = axes[2], sort = False)
+	axes[2].set_title('Month Means for Energy')
+
+
+	fig.suptitle('Energy Means by Different Time Groups')
+	plt.tight_layout()
+	plt.savefig(loc + 'energy_time_means.png')
+	plt.clf()
+
 	### VISUALIZATION of DATA MEDIANS ###
 	plot_df = pd.read_csv(cwd + direc + params['out_name'])
 
